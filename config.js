c364e93b6a9cc5fa9dc196ef782bb383f9373db7
Donovan Le <dtle647@gmail.com>
2021-02-13 14:40:57 -0600

    initial commit

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET,
-  NODE_ENV: process.env.NODE_ENV,
-  DB_URL: process.env.DB_URL,
-  LOCAL_DB_URL: process.env.LOCAL_DB_URL,
-  TEST_DB_URL: process.env.TEST_DB_URL
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    LOCAL_DATABASE_URL: process.env.LOCAL_DATABASE_URL,
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 };

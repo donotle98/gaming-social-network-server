186c84d19976b4df40037ad691eaddc28cd423ca
Donovan Le <dtle647@gmail.com>
2021-02-13 14:48:32 -0600

    initial commit

--- config.js
+++ config.js
@@ -4,6 +4,5 @@ module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    LOCAL_DATABASE_URL: process.env.LOCAL_DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 };
